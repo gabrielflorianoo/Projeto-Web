4565180bfaac9dd2079786b44c397b9a2dcae738
gabrielflo <88804052+gabrielflorianoo@users.noreply.github.com>
2024-04-25 20:30:26 -0300

    Add files via upload

--- routes/logout.js
+++ routes/logout.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.render('logout');
+    if (req.body.sair == 'true') {
+        res.redirect('/');
+    } else if (req.body.sair == 'false') {
+        res.redirect('/user');
+    }
+    res.render('logout');
 });
 
 module.exports = router;
