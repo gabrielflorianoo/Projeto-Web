e3d8c26e2864a3b99742430ff99beba75a4f1f58
gabrielflo <88804052+gabrielflorianoo@users.noreply.github.com>
2024-04-25 20:44:21 -0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user');
+  res.render('user', {logado: req.session.user ? true : false});
 });
 
 module.exports = router;
