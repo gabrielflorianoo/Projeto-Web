359eef5569b07e9d92a2bdd3cea6d26afdef2a86
gabrielflo <88804052+gabrielflorianoo@users.noreply.github.com>
2024-04-25 20:11:03 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const adminRouter = require('./routes/admin');
+const logoutRouter = require('./routes/logout');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/admin', adminRouter);
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
