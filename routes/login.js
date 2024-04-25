ecfb97bcc88b919919e57a98bf6942ebd042d4c1
gabrielflo <88804052+gabrielflorianoo@users.noreply.github.com>
2024-04-25 19:57:31 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const validador = require("../validators/Login.js");
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  
+  if (validador.validate(req.body) == false) {
+    res.send("Usuário não criado!");
+  } else {
+    res.send("Usuário criado!")
+  }
 });
 
 module.exports = router;
